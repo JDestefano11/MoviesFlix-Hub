9fd5bbc13180e7d8ef360514d0ece74918cc6ca5
DesJCodes <141986568+DesJCodes@users.noreply.github.com>
2024-05-09 16:22:40 -0400

    Add: Added different code into each of the 3 new created files. - Inside index.jsx: 1. Added the basic setup for a react app. 2. Imported the createRoot from react-dom/client & imported the SCSS file. 3. Defined the MyFlixApplication component. Inside index.html: 1. Added the essential parts needed to load and display the application. Inside index.scss: 1. Added a test color to the .my-flix class selector.

---
+++ myFlix-client/src/index.jsx
@@ -0,0 +1,20 @@
+import { createRoot } from 'react-dom/client'; 
+
+import "./index.scss"; 
+
+// Main Component
+const MyFlixApplication = () => {
+    return (
+        <div className="my-flix">
+            <div>Good morning</div>
+        </div>
+    );
+};
+
+// Finds the root of app 
+const container = document.querySelector("root"); 
+const root = createRoot(container); 
+
+// Tell React to Render app in the root Dom element
+root.render(<MyFlixApplication />); 
+
