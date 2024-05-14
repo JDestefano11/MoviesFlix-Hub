a2dcc682e00a1b8c7592df25b4116f061db4c5b5
JDestefanoCodes <158540954+JDestefanoCodes@users.noreply.github.com>
2024-05-14 12:42:46 -0700

    Add & Create: - Created a movie-card folder with a movie-card.jsx file in it -Created a movie-view folder with a movie-view file in it. - Inside movie-card.jsx added the export of the component to main-view.jsx. - Added the movies data inside of the main-view component & exported the MainView

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,11 +1,29 @@
+import { useState } from "react";
+import { MovieCard } from "../movie-card/movie-card";
+
 export const MainView = () => {
+    const [movies, setMovies] = useState([
+        { id: 1, title: "Jurassic Park" },
+        { id: 2, title: "The Dark Night" },
+        { id: 3, title: "Schindler/'s List" },
+        { id: 4, title: "Inception" },
+        { id: 5, title: "The Shawshank Redemption" },
+        { id: 6, title: "Forest Gump" },
+        { id: 7, title: "The Godfather" },
+        { id: 8, title: "The Matrix" },
+        { id: 9, title: "Gladiator" },
+        { id: 10, title: "Pulp Fiction" }
+    ]);
+
+    if (movies.length === 0) {
+        return <div>The movie list is empty</div>
+    }
     return (
         <div>
-            <div>Eloquent JavaScript</div>
-            <div>Mastering JavaScript Functional Programming</div>
-            <div>JavaScript: The Good Parts</div>
-            <div>JavaScript: The Definitive Guide</div>
-            <div>The Road to React</div>
+            {movies.map((movie) => {
+                <MovieCard />
+            })}
         </div>
     );
-}
+};
+
