3915e2bd6e5587eaa3a5beed7fe1b122c8e48230
JDestefanoCodes <158540954+JDestefanoCodes@users.noreply.github.com>
2024-06-13 20:06:16 -0700

    "Added PropTypes to MovieCard component"

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import PropTypes from 'prop-types';
 
 export const MovieCard = ({ movie, onClick }) => {
+    console.log("Type of onClick:", typeof onClick);
     return (
         <div onClick={() => onClick(movie)}>
             <h3>{movie.Title}</h3>
@@ -8,3 +9,9 @@ export const MovieCard = ({ movie, onClick }) => {
     );
 };
 
+MovieCard.propTypes = {
+    movie: PropTypes.shape({
+        Title: PropTypes.string.isRequired
+    }).isRequired,
+    onClick: PropTypes.func.isRequired
+};
