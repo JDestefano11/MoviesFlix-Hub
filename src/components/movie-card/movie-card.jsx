de89d07f7850a6b4a7b1eedbf6533f7e77096941
JDestefanoCodes <158540954+JDestefanoCodes@users.noreply.github.com>
2024-06-13 23:18:19 -0700

    "Renamed prop 'onClick' to 'onMovieClick' in MovieCard component and updated MovieView component to use 'movie' prop instead of 'movieData'."

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,9 +1,6 @@
-
-
-export const MovieCard = ({ movie, onClick }) => {
-    console.log("Type of onClick:", typeof onClick);
+export const MovieCard = ({ movie, onMovieClick }) => {
     return (
-        <div onClick={() => onClick(movie)}>
+        <div onClick={() => onMovieClick(movie)}>
             <h3>{movie.Title}</h3>
         </div>
     );
